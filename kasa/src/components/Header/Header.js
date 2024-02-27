a3daa77c13ee52d9ed38a1042bd53d140407b392
Christopher <christopher.cornet@laplateforme.io>
2024-02-28 00:20:06 +0100

    Set underline on links based on current path

--- kasa/src/components/Header/Header.js
+++ kasa/src/components/Header/Header.js
@@ -1,15 +1,18 @@
 import styles from './Header.module.scss';
 import logoKasa from '../../images/logoKasa.png';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 function Header() {
+    const location = useLocation();
+
     return (
         <header>
             <img src={logoKasa} className={styles.logoKasa} alt="Kasa" />
             <nav>
                 <ul className={styles.ul}>
-                    <li><Link to="/" className={styles.homeLink}>Accueil</Link></li>
-                    <li><Link to="/a-propos" className={styles.aboutLink}>A Propos</Link></li>
+                    <li><Link to="/" className={location.pathname === "/" ?  styles.homeLink_CurrentPath : styles.homeLink}>Accueil</Link></li>
+                    <li><Link to="/a-propos" className={location.pathname === "/a-propos" ?  styles.aboutLink_CurrentPath : styles.aboutLink}>A Propos</Link></li>
                 </ul>
             </nav>
         </header>
