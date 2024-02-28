61db1985a487f785fa6c403d32b3f70086892e75
Christopher <christopher.cornet@laplateforme.io>
2024-02-28 03:49:10 +0100

    Start of the "Fiche logement" part

---
+++ kasa/src/components/Gallery/Gallery.js
@@ -0,0 +1,23 @@
+import styles from './Gallery.module.scss';
+import data from '../../logements.json';
+// import { useLocation } from 'react-router-dom';
+import arrowLeft from '../../images/arrowLeft.png';
+import arrowRight from '../../images/arrowRight.png';
+
+function Gallery() {
+    console.log(data);
+
+    // const location = useLocation();
+
+    return (
+        <div className={styles.bannerContainer}>
+            <img className={styles.banner} src={data[0].cover} alt="Appartement à louer" />
+            <div className={styles.arrows}>
+                {/* <img className={styles.arrow} src={arrowLeft} alt="Flèche gauche" />
+                <img className={styles.arrow} src={arrowRight} alt="Flèche droite" /> */}
+            </div>
+        </div>
+    );
+}
+
+export default Gallery;
--- kasa/src/pages/Fiche-Logement/Fiche-Logement.js
+++ kasa/src/pages/Fiche-Logement/Fiche-Logement.js
@@ -1,9 +1,44 @@
 import styles from './Fiche-Logement.module.scss';
+import Gallery from '../../components/Gallery/Gallery';
+import data from '../../logements.json';
+// import { useLocation } from 'react-router-dom';
+import Collapse from '../../components/Collapse/Collapse';
 
 function Logement() {
     return (
-        <div>
-            <h1 className={styles.title}>Logement</h1>
+        <div className={styles.ficheLogement}>
+            <Gallery />
+
+            <main className={styles.main}>
+                <section className={styles.informationsContainer}>
+                    <div>
+                        <h1 className={styles.bannerTitle}>{data[0].title}</h1>
+                        <h2 className={styles.location}>{data[0].location}</h2>
+
+                        <div className={styles.tagContainer}>
+                            <div className={styles.tags} key={data[0].id}>
+                                <p className={styles.tag}>{data[0].tags}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.informationsHost}>
+                        <h1 className={styles.hostName}>{data[0].host.name}</h1>
+                        <img className={styles.hostPicture} src={data[0].host.picture} alt="Propriétaire de l'appartement" />
+                    </div>
+                </section>
+
+                <section className={styles.details}>
+                    <Collapse
+                        title="Description"
+                        text={data[0].description}
+                    />
+                    <Collapse
+                        title="Équipements"
+                        text={data[0].equipments} // /!\ Je dois mapper les équipements
+                    />
+                </section>
+            </main>
         </div>
     );
 }
