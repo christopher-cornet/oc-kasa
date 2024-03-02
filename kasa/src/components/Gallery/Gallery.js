f0d18a02598b75289881fb6b3da05214961b86cf
Christopher <christopher.cornet@laplateforme.io>
2024-03-02 23:07:10 +0100

    Add dynamic informations

--- kasa/src/components/Gallery/Gallery.js
+++ kasa/src/components/Gallery/Gallery.js
@@ -1,29 +1,36 @@
 import styles from './Gallery.module.scss';
 import data from '../../logements.json';
-// import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import arrowLeft from '../../images/arrowLeft.png';
 import arrowRight from '../../images/arrowRight.png';
 
 function Gallery() {
     console.log(data);
 
-    // const location = useLocation();
+    let currentPicture = 1;
+    // let maxPicture = data.length;
 
-    let arrowLeft = () => {
+    const location = useLocation();
 
-    }
+    console.log(location.pathname);
 
-    let arrowRight = () => {
-
+    let handleClick = (arrow) => {
+        if (arrow === 'left') {
+            console.log('left');
+        }
+        else if (arrow === 'right') {
+            console.log('right');
+        }
     }
 
     return (
         <div className={styles.bannerContainer}>
             <img className={styles.banner} src={data[0].cover} alt="Appartement à louer" />
             <div className={styles.arrows}>
-                <img className={styles.arrow} onClick={arrowLeft} src={arrowLeft} alt="Flèche gauche" />
-                <img className={styles.arrow} onClick={arrowRight} src={arrowRight} alt="Flèche droite" />
+                <img className={styles.arrow} onClick={() => handleClick('left')} src={arrowLeft} alt="Flèche gauche" />
+                <img className={styles.arrow} onClick={() => handleClick('right')} src={arrowRight} alt="Flèche droite" />
             </div>
+            <p className={styles.pictureNumber}>{pictureNumber}</p>
         </div>
     );
 }
